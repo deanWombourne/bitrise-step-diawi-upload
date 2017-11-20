d844a1bd28e2d65865940345cfef7d0d76620cae
Krisztian Dobmayer <krisztian.dobmayer@bitrise.io>
2017-11-20 11:46:59 +0100

    Update step

--- step.sh
+++ step.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-set -ex
+#!/usr/bin/env bash
+#
+# Uploads an ipa to Diawi
+set -e
 
-set -o pipefail
-
-npm install -g diawi
-
-URL=`diawi upload ${api_token} ${filename}`
-
-envman add --key DIAWI_UPLOAD_URL --value $URL
+which diawi >/dev/null 2>&1 || npm install -g diawi
+diawi_url=$(diawi upload "${api_token}" "${filename}")
+envman add --key DIAWI_UPLOAD_URL --value "${diawi_url}"
