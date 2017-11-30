5814a065056257a2af37ae12cdade6ff051690f9
Adam Hoyle <atomoil@users.noreply.github.com>
2017-11-30 14:26:15 +0000

    Log the Diawi upload URL (in case future steps fail etc) (#2)
    * added logging of diawi URL

--- step.sh
+++ step.sh
@@ -5,4 +5,5 @@ set -e
 
 which diawi >/dev/null 2>&1 || npm install -g diawi
 diawi_url=$(diawi upload "${api_token}" "${filename}")
+echo Diawi Upload URL is $diawi_url
 envman add --key DIAWI_UPLOAD_URL --value "${diawi_url}"
